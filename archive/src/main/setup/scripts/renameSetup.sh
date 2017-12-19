fc5cba3990a30ef8c584d0206d011b594a4acf86
Robin Weiß <row@informatik.uni-kiel.de>
2017-12-19 10:30:38 +0100

    SAI-332 Changed the escape characters of sed commands to ~ Added comment to gitignore

--- archive/src/main/setup/scripts/renameSetup.sh
+++ archive/src/main/setup/scripts/renameSetup.sh
@@ -3,7 +3,7 @@ echo "Renaming setup files"
 # function definitions
 
 GetProviderClassName () {
- echo "$1" | sed -e "s/[$2]\(\\w\)/\U\1/g" -e "s/[$2]//g" -e "s/^\(\\w\)/\U\1/g"
+ echo "$1" | sed -e "s~[$2]\(\\w\)~\U\1~g" -e "s~[$2]~~g" -e "s~^\(\\w\)~\U\1~g"
 }
 
 
@@ -47,7 +47,7 @@ ProcessFilesInDir () {
 
 
 RenameFile  () {
- renamedFile=$(echo $1 | sed -e "s/\${providerClassName}/${providerClassName}/g" -e "s/\${void}//g")
+ renamedFile=$(echo $1 | sed -e "s~\${providerClassName}~${providerClassName}~g" -e "s~\${void}~~g")
  if [ "$renamedFile" != "$1" ]; then
    mv -f $1 $renamedFile
    echo "Renamed $1 to $renamedFile" >&2
@@ -57,7 +57,7 @@ RenameFile  () {
 
 RenameDirectory  () {
  originalDir=$(realpath $1)
- renamedDir=$(realpath -q $(echo $1 | sed -e "s/\${providerPackageName}/${providerPackageName}/g"))
+ renamedDir=$(realpath -q $(echo $1 | sed -e "s~\${providerPackageName}~${providerPackageName}~g"))
  if [ "$originalDir" != "$renamedDir" ]; then
    mv -f $originalDir $renamedDir
    echo "Renamed $1 to $renamedDir" >&2
@@ -67,15 +67,15 @@ RenameDirectory  () {
 
 
 RenameFileContent  () {
- sed --in-place=.tmp -e "s/\${providerPackageName}/${providerPackageName}/g" \
-     --in-place=.tmp -e "s/\${providerClassName}/${providerClassName}/g" \
-     --in-place=.tmp -e "s/\${providerName}/${providerName}/g" \
-     --in-place=.tmp -e "s/\${providerUrl}/${providerUrl}/g" \
-     --in-place=.tmp -e "s/\${authorFullName}/${authorFullName}/g" \
-     --in-place=.tmp -e "s/\${authorEmail}/${authorEmail}/g" \
-     --in-place=.tmp -e "s/\${authorOrganization}/${authorOrganization}/g" \
-     --in-place=.tmp -e "s/\${authorOrganizationUrl}/${authorOrganizationUrl}/g" \
-     --in-place=.tmp -e "s/\${parentHarvesterVersion}/${parentHarvesterVersion}/g" $1 && rm -f $1.tmp
+ sed --in-place=.tmp -e "s~\${providerPackageName}~${providerPackageName}~g" \
+     --in-place=.tmp -e "s~\${providerClassName}~${providerClassName}~g" \
+     --in-place=.tmp -e "s~\${providerName}~${providerName}~g" \
+     --in-place=.tmp -e "s~\${providerUrl}~${providerUrl}~g" \
+     --in-place=.tmp -e "s~\${authorFullName}~${authorFullName}~g" \
+     --in-place=.tmp -e "s~\${authorEmail}~${authorEmail}~g" \
+     --in-place=.tmp -e "s~\${authorOrganization}~${authorOrganization}~g" \
+     --in-place=.tmp -e "s~\${authorOrganizationUrl}~${authorOrganizationUrl}~g" \
+     --in-place=.tmp -e "s~\${parentHarvesterVersion}~${parentHarvesterVersion}~g" $1 && rm -f $1.tmp
 }
 
 
